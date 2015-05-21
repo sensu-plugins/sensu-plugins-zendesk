1c1551dc82c2465495b643592087ebbc9cff91d2
Matt Jones <mattjones@yieldbot.com>
2015-05-21 12:12:56 -0400

    update repo

--- lib/sensu-plugins-zendesk.rb
+++ lib/sensu-plugins-zendesk.rb
@@ -1,7 +1,15 @@
+
+require 'sensu-plugins-wordpress/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
-module SensuPluginsZendesk
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+module SensuPluginsWordpress
+  class << self
+  end
+
+  class << self
+  end
 end
